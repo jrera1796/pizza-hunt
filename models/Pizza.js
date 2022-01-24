6e393d890bbf8bc38f2c2f3be86b035f228283ff
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2022-01-23 19:53:55 -0800

    Finished comment model and linked to front end

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require('mongoose');
-
+const dateFormat = require('../utils/dateFormat');
 const PizzaSchema = new Schema({
   pizzaName: {
     type: String
@@ -9,7 +9,9 @@ const PizzaSchema = new Schema({
   },
   createdAt: {
     type: Date,
-    default: Date.now
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal)
+
   },
   size: {
     type: String,
@@ -26,6 +28,7 @@ const PizzaSchema = new Schema({
   {
     toJSON: {
       virtuals: true,
+      getters: true
     },
     id: false
   });
